cac751c1b9bcc008f828e99c8f301d56c7a13e44
Endre Mo <endrehm@stud.ntnu.no>
2023-05-30 05:57:17 +0200

    Allows converter to handle multiple files

--- ccsl-workspace/org.ccsl.converter/src/resourceConverter/Main2.java
+++ ccsl-workspace/org.ccsl.converter/src/resourceConverter/Main2.java
@@ -1,8 +1,11 @@
 package resourceConverter;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Iterator;
-
+import java.io.File;
+import java.nio.file.Paths;
 
 import ccsl.ccslPackage;
 import org.ccsl.TextualStandaloneSetup;
@@ -18,53 +21,95 @@ import org.eclipse.xtext.resource.XtextResourceSet;
 import com.google.inject.Injector;
 
 public class Main2 {
-	private static int uniquenameCounter = 1;
+private static int uniquenameCounter = 1;
 	
-    public static void main(String[] args) {
-    	ccslPackage.eINSTANCE.eClass();
+	public static void main(String[] args) {
+		ccslPackage.eINSTANCE.eClass();
 
-        Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ccsl", new XMIResourceFactoryImpl());
-
-        ResourceSet resourceSet = new ResourceSetImpl();
-        /*URI inputURI = URI.createFileURI("C:/skole/MasterThesis/Workspace/org.ccsl.metamodel.examples/PMD/PMD-ErrorProne/CompareObjectsWithEquals.ccsl");*/
-        URI inputURI = URI.createFileURI("C:/skole/MasterThesis/Workspace/org.ccsl.metamodel.examples/sample-instances/anonymousClass.ccsl");
-        /*URI inputURI = URI.createFileURI("C:/skole/MasterThesis/Workspace/org.ccsl.metamodel.examples/PMD/PMD-ErrorProne/NonCaseLabelInSwitchStatement.ccsl");*/
-        /*URI inputURI = URI.createFileURI("C:/skole/MasterThesis/Workspace/org.ccsl.metamodel.examples/PMD/PMD-ErrorProne/MissingSerialVersionUID.ccsl");*/
-        /*URI inputURI = URI.createFileURI("C:/skole/MasterThesis/Workspace/org.ccsl.metamodel.examples/PMD/PMD-ErrorProne/EmptyFinallyBlock.ccsl");*/
-        /*URI inputURI = URI.createFileURI("C:/skole/MasterThesis/Workspace/org.ccsl.metamodel.examples/PMD/PMD-ErrorProne/AvoidDecimalLiteralsInBigDecimalConstructor.ccsl");*/
-        /*URI inputURI = URI.createFileURI("C:/skole/MasterThesis/Workspace/org.ccsl.metamodel.examples/PMD/PMD-ErrorProne/UnnecessaryBooleanAssertion.ccsl");*/
-        /*URI inputURI = URI.createFileURI("C:/skole/MasterThesis/Workspace/org.ccsl.metamodel.examples/PMD/PMD-Performance/TooFewBranchesForASwitchStatement.ccsl");*/
-        Resource xmiResource = resourceSet.getResource(inputURI, true);
-        
-        EObject topObject = xmiResource.getContents().get(0);
-        
-        changeUniqueName(topObject);
-
-        try {
-            xmiResource.save(null);
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (args.length < 1) {
+            System.out.println("Please provide the directory path as argument.");
+            return;
         }
-        
-        nameToString(topObject);
 
-        Injector injector = new TextualStandaloneSetup().createInjectorAndDoEMFRegistration();
-        XtextResourceSet xtextResourceSet = injector.getInstance(XtextResourceSet.class);
+        String directoryPath = args[0];
         
-
-        URI outputURI = URI.createFileURI("C:/skole/MasterThesis/Workspace/org.ccsl.converter/testRes/converted.ccslt");
-        XtextResource xtextResource = (XtextResource) xtextResourceSet.createResource(outputURI);
-        
-        
-
-        xtextResource.getContents().add(topObject);
-
         try {
-            xtextResource.save(null);
+            Files.walk(Paths.get(directoryPath))
+                .filter(Files::isRegularFile)
+                .map(Path::toString)
+                .filter(path -> path.endsWith(".ccsl"))
+                .forEach(Main2::convertFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+	private static void convertFile(String filePath) {
+		try {
+			System.out.println("Processing file: " + filePath);
+		    URI inputURI = URI.createFileURI(filePath);
+		    System.out.println("Input URI: " + inputURI);
+		    ResourceSet xmiResourceSet = new ResourceSetImpl();
+		    xmiResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("ccsl", new XMIResourceFactoryImpl());
+		    Resource xmiResource = xmiResourceSet.getResource(inputURI, true);
+	
+		    if (!xmiResource.getErrors().isEmpty()) {
+		        System.out.println("Resource loading errors: " + xmiResource.getErrors());
+		        return;
+		    }
+		        
+		    EObject topObject;
+		    try {
+		        topObject = xmiResource.getContents().get(0);
+		    } catch (IndexOutOfBoundsException e) {
+		        System.out.println("An error occurred while processing file: " + filePath);
+		        e.printStackTrace();
+		        return;
+		    }
+		        
+		    changeUniqueName(topObject);
+	
+		    /*try {
+		        xmiResource.save(null);
+		    } catch (IOException e) {
+		        e.printStackTrace();
+		    }*/
+		        
+		    nameToString(topObject);
+	
+		    Injector injector = new TextualStandaloneSetup().createInjectorAndDoEMFRegistration();
+		    XtextResourceSet xtextResourceSet = injector.getInstance(XtextResourceSet.class);
+		        
+		    File file = new File(filePath);
+		    
+		    
+		    String fileName = file.getName();
+		    String fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf("."));
+		    
+		    
+		    String parentDir = file.getParent();
+	
+		    
+		    String outputFilePath = parentDir.replace("org.ccsl.metamodel.examples", "org.ccsl.converter/testRes") 
+		                        + "/" + fileNameWithoutExtension + ".ccslt";
+	
+		    URI outputURI = URI.createFileURI(outputFilePath);
+		    System.out.println("Output URI: " + outputURI);
+		    XtextResource xtextResource = (XtextResource) xtextResourceSet.createResource(outputURI);
+	
+		    xtextResource.getContents().add(topObject);
+	
+		    try {
+		    	System.out.println("File saved: " + outputURI);
+		        xtextResource.save(null);
+		    } catch (IOException e) {
+		        e.printStackTrace();
+		    }
+		} catch (Exception e) {
+	        System.err.println("An error occurred while processing file: " + filePath);
+	        e.printStackTrace();
+	    }
+	}
         public static void changeUniqueName(EObject eObject) {
             for (Iterator<EObject> it = eObject.eAllContents(); it.hasNext();) {
                 EObject childEObject = it.next();
